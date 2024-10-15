3f6cbc2b0ca84fb249d57984a7db9a84d431c5b8
Daniklo Sovano <danilosovano@gmail.com>
2024-10-15 16:49:43 -0300

    add limpar terminal e tabuleiro de para representar as jogadas

--- Batalha-naval.py
+++ Batalha-naval.py
@@ -1,20 +1,27 @@
 import random
 import string 
 import time
+import os 
 
-tabuleiro_player1 = [["-" for _ in range(10)] for _ in range(10)]
-tabuleiro_player2 = [["-" for _ in range(10)] for _ in range(10)]
+def criar_tabuleiro():
+    return [["-" for _ in range(10)] for _ in range(10)]
 
-def ataque(player, oponente):
+def limpar_terminal():
+    os.system("cls")
+
+def ataque(player, navios, oponente):
+    
     print(f"Sua vez {player}, faça sua jogada! \n")
+    
     jogada_linha = input("Digite a Linha desejada: ").upper()
+    
     while jogada_linha not in string.ascii_uppercase[:10]:
         print("\nLinha invalida! Digite uma letra de A a J.")
         jogada_linha = input("Digite a Linha desejada: ").upper()
 
     jogada_linha = string.ascii_uppercase.index(jogada_linha)  # Converte a letra para o índice (0 a 9)
-    
     jogada_coluna = int(input("Digite a coluna desejada: ")) - 1
+    
     while jogada_coluna < 0 or jogada_coluna > 9:
         print("\nColuna inválida! Digite um número entre 1 e 10.")
         jogada_coluna = int(input("Digite a coluna desejada (1-10): ")) - 1
@@ -33,38 +40,34 @@ def ataque(player, oponente):
           _________|_ __ |_________
         """)
         oponente[jogada_linha][jogada_coluna] = "X"
+        navios[jogada_linha][jogada_coluna] = "X"
+        
+
     else: 
         print("\nAcerto na água!")
-        oponente[jogada_linha][jogada_coluna] = "O"
+        navios[jogada_linha][jogada_coluna] = "O"
     time.sleep(1)  # Pausa antes de continuar para o próximo jogador
 
 
 # Navios de uma posição
-def navios_pequenos(tabuleiro_player): 
-    navios_colocados = 0
-    while navios_colocados < 5:
+def adicionar_navios(tabuleiro_player,navios_pequenos = 5, navios_grandes= 3): 
+    for i in range(navios_pequenos):
         x = random.randint(0,9)
         y = random.randint(0,9)
         if tabuleiro_player[x][y] == "-":
             tabuleiro_player[x][y] = 'N'
-            navios_colocados += 1
+            
         
-# Navios de três posições
-def navios_grandes(tabuleiro_player): 
-    for i in range(3):
+    for i in range(navios_grandes):
         while True:
             x = random.randint(0,9)
-            y = random.randint(0,9)
-            if y <= 7:  # verifica se o navio cabe na coluna
+            y = random.randint(0,7)
+            if tabuleiro_player[x][y] == "-" and tabuleiro_player[x][y+1] == "-" and tabuleiro_player[x][y+2] == "-":
                 tabuleiro_player[x][y] = "G"
                 tabuleiro_player[x][y+1] = "G"
                 tabuleiro_player[x][y+2] = "G"
                 break
-            else:  # move o navio para trás caso não caiba  
-                tabuleiro_player[x][y] = "G"
-                tabuleiro_player[x][y-1] = "G"
-                tabuleiro_player[x][y-2] = "G"
-                break
+
 
 def imprimir_tabuleiro(tabuleiro):
     linhas = string.ascii_uppercase[:10]  # Letras de A a J para as linhas
@@ -81,26 +84,34 @@ def imprimir_tabuleiro(tabuleiro):
         print()  # Quebra de linha após cada linha
     time.sleep(2)  # Pausa para o jogador visualizar o tabuleiro
 
-# Adiciona os navios ao tabuleiro
-navios_grandes(tabuleiro_player1)
-navios_pequenos(tabuleiro_player1)
-navios_grandes(tabuleiro_player2)
-navios_pequenos(tabuleiro_player2)
+tabuleiro_player1 = criar_tabuleiro()
+tabuleiro_player2 = criar_tabuleiro()
 
-# Loop de ataque entre os jogadores
+ataque_player1 = criar_tabuleiro()
+ataque_player2 = criar_tabuleiro()
+adicionar_navios(tabuleiro_player1)
+adicionar_navios(tabuleiro_player2)
+
 while True:
-    print("   Tabuleiro Player 2")
-    imprimir_tabuleiro(tabuleiro_player2)
-
-    ataque("Player 1", tabuleiro_player2)  # Player 1 ataca o tabuleiro do Player 2
-    imprimir_tabuleiro(tabuleiro_player2)
-
-    print("   Tabuleiro Player 1")
+    limpar_terminal()
+    print("Tabuleiro de navios Player1")
     imprimir_tabuleiro(tabuleiro_player1)
 
-    ataque("Player 2", tabuleiro_player1)  # Player 2 ataca o tabuleiro do Player 1
-    imprimir_tabuleiro(tabuleiro_player1)
+    print("\nTabuleiro de ataque Player1")
+    imprimir_tabuleiro(ataque_player1)
 
+    ataque("player1",ataque_player1, tabuleiro_player2)
+    
+    limpar_terminal()
+    time.sleep(2)
+    print("\nTabuleiro de navios Player2")
+    imprimir_tabuleiro(tabuleiro_player2)
+
+    print("\nTabuleiro de ataque Player2")
+    imprimir_tabuleiro(ataque_player2)
+    
+    ataque("player2",ataque_player2, tabuleiro_player1)
+    
 # print("""
         ##########################          
         ##########################          
@@ -132,4 +143,4 @@ while True:
             ##################              
           ######################            
          ########################           
-      """)
+#      """)
